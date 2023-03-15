b9d437caa2f9190c3c026bb879db03fecf4067a9
Jacob Firek <jacob@interlay.io>
2023-03-15 17:27:19 -0400

    concurrency work

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,9 +1,9 @@
+use hello::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
-use hello::ThreadPool;
 
 fn main() {
     // listener to TCP connects on the IP:port(127.0.0.1:787)
@@ -22,7 +22,7 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename ) = if request_line == "GET / HTTP/1.1" {
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
@@ -30,8 +30,6 @@ fn handle_connection(mut stream: TcpStream) {
 
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
-    let response = format!(
-        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-    );
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
 }
