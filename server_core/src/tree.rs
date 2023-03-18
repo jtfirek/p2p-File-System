3d7af04905ef4ca21cc9ebe8673efff5fcb4dc97
Jacob Firek <jacob@interlay.io>
2023-03-18 16:56:20 -0400

    readme updated

--- server_core/src/tree.rs
+++ server_core/src/tree.rs
@@ -1,14 +1,10 @@
-use std::sync::atomic::{AtomicUsize, Ordering};
 
-/// A generic tree node that holds a value of type `T`, a unique identifier, and a list of its children.
+/// A generic tree node that holds a value of type `T` and a list of its children.
 pub struct TreeNode<T> {
-    id: usize,
     pub value: T,
     pub children: Vec<TreeNode<T>>,
 }
 
-/// A global atomic counter used to generate unique identifiers for tree nodes.
-static NODE_ID_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
 impl<T> TreeNode<T> {
     /// Creates a new `TreeNode` with the given value and no children.
@@ -21,9 +17,7 @@ impl<T> TreeNode<T> {
     ///
     /// A new `TreeNode` instance with the given value, a unique identifier, and no children.
     pub fn new(value: T) -> Self {
-        let id = NODE_ID_COUNTER.fetch_add(1, Ordering::SeqCst);
         TreeNode {
-            id,
             value,
             children: Vec::new(),
         }
